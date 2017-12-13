ba9a4d18fea0bc7868a3ea7fec8d4ff69b160454
Vanessa <tvanessa@users.noreply.github.com>
2017-12-12 21:57:10 -0800

    Generate stereo image

--- main.cpp
+++ main.cpp
@@ -7,10 +7,11 @@
 #include <glm/gtx/vector_angle.hpp>
 #include <glm/gtx/rotate_vector.hpp>
 
-const glm::vec3 EYE = glm::vec3(0.0, -10.0, 20.0);
+const glm::vec3 EYE = glm::vec3(-0.125, -10.0, 20.0);
 const float FIELD_OF_VIEW = M_PI * 0.6;
-const int IMAGE_SIZE = 500;
+const int IMAGE_SIZE = 800;
 const int Z_LIM = 100;
+const float EYE_FACTOR = 0.25f;
 
 float flmod(float fl, float m) {
     return fl - m * floor(fl / m);
@@ -23,17 +24,17 @@ float userFunction(float x, float y, float z) {
     return z > 0.0f ? x*x + y*y + z*z - 70.0f : z; // Esfera
 }
 
-float evaluateUserFunction(glm::vec3 ray, float multiplier) {
-    glm::vec3 pointInRay = EYE + ray * multiplier;
+float evaluateUserFunction(glm::vec3 eye, glm::vec3 ray, float multiplier) {
+    glm::vec3 pointInRay = eye + ray * multiplier;
     return userFunction(pointInRay.x, pointInRay.y, pointInRay.z) ;
 }
 
-float findFunctionRoot(glm::vec3 ray, float pointOnPositiveSide, float pointOnNegativeSide) {
+float findFunctionRoot(glm::vec3 eye, glm::vec3 ray, float pointOnPositiveSide, float pointOnNegativeSide) {
     assert(pointOnNegativeSide >= pointOnPositiveSide);  
     float mid ;
     for (int step = 0; step < 55; ++step) { //52bits
         mid = (pointOnNegativeSide + pointOnPositiveSide) / 2 ;
-        if (evaluateUserFunction(ray, mid) < 0) 
+        if (evaluateUserFunction(eye, ray, mid) < 0) 
             pointOnNegativeSide = mid ; 
         else
             pointOnPositiveSide = mid ;
@@ -41,21 +42,21 @@ float findFunctionRoot(glm::vec3 ray, float pointOnPositiveSide, float pointOnNe
     return mid;
 }
 
-int parity(float a) {
-    int intPart = (int)floor(a) ;
-    return !(intPart % 2 == 0);
+int squareOfIntPart(float a) {
+    int intPart = (int)floor(a);
+    return intPart * intPart;
 }
 
-bool castRay(glm::vec3 ray) {
+int castRay(glm::vec3 eye, glm::vec3 ray) {
     // Encontrar puntos en la trayectoria del rayo tales que userFunction(ray * pointOnPositiveSide) > 0 y userFunction(ray * pointOnNegativeSide) < 0.
-    assert(evaluateUserFunction(EYE, 0.0f) > 0.0f and "EYE should be on positive side of function.");  
+    assert(evaluateUserFunction(eye, ray, 0.0f) > 0.0f and "eye should be on positive side of function.");  
     int iterations = 500 ;
     float pointOnNegativeSide = Z_LIM;
-    bool findNegative = evaluateUserFunction(ray, pointOnNegativeSide) < 0.0f;
+    bool findNegative = evaluateUserFunction(eye, ray, pointOnNegativeSide) < 0.0f;
 
     while (iterations--) {
         float randomMultiplier = ((float) rand() / RAND_MAX) * pointOnNegativeSide;
-        if (evaluateUserFunction(ray, randomMultiplier) < 0) {
+        if (evaluateUserFunction(eye, ray, randomMultiplier) < 0) {
             pointOnNegativeSide = randomMultiplier;
             findNegative = true;
         }
@@ -63,36 +64,24 @@ bool castRay(glm::vec3 ray) {
     if (not findNegative){
         return false;
     }
-    float functionRoot = findFunctionRoot(ray, 0.0f /* pointOnPositiveSide */, pointOnNegativeSide);
-    glm::vec3 root = EYE + ray * ((float)functionRoot) ; // Busqueda binaria.
-    return parity(root.x) == parity(root.y); // Regresar falso si cae en cuadrito blanco, verdadero si cae en cuadrito negro.
+    float functionRoot = findFunctionRoot(eye, ray, 0.0f /* pointOnPositiveSide */, pointOnNegativeSide);
+    glm::vec3 root = eye + ray * ((float)functionRoot) ; // Busqueda binaria.
+    return squareOfIntPart(root.x) + squareOfIntPart(root.y); // Regresar falso si cae en cuadrito blanco, verdadero si cae en cuadrito negro.
 }
 
-int main(int argc, char** argv) {
-    srand(time(NULL)) ;
-    // Compute coordinates of the corners of the virtual screen.
-    glm::vec3 dir = glm::normalize(-EYE);
-    glm::vec3 left = glm::normalize(glm::cross(dir, glm::vec3(0.0f, 1.0f, 0.0f)));
-    glm::vec3 up = glm::normalize(glm::cross(dir, left));
-    glm::vec3 topLeft = EYE + dir - left - up;
-    glm::vec3 topRight = EYE + dir + left - up;
-    glm::vec3 bottomLeft = EYE + dir - left + up;
-    glm::vec3 bottomRight = EYE + dir + left + up;
-
-    // Cast rays onto function and generate "chess board" image.
-    bool chessBoard[IMAGE_SIZE][IMAGE_SIZE];
+void rayTracing(int chessBoard[IMAGE_SIZE][IMAGE_SIZE], glm::vec3 eye, glm::vec3 topLeft, glm::vec3 topRight, glm::vec3 bottomLeft) {
     for (int pixelY = 0; pixelY < IMAGE_SIZE; pixelY ++) {
         for (int pixelX = 0; pixelX < IMAGE_SIZE; pixelX ++) {
             glm::vec3 pixelReference =
                     topLeft + (topRight - topLeft) * ((float) pixelX / IMAGE_SIZE )
                             + (bottomLeft - topLeft) * ((float) pixelY / IMAGE_SIZE );
-            glm::vec3 pixelRay = glm::normalize(pixelReference - EYE);
-            chessBoard[pixelY][pixelX] = castRay(pixelRay);
+            glm::vec3 pixelRay = glm::normalize(pixelReference - eye);
+            chessBoard[pixelY][pixelX] = castRay(eye, pixelRay);
         }
     }
-  
-    // Detect borders on "chess board".
-    bool bordersImage[IMAGE_SIZE][IMAGE_SIZE];
+}
+
+void detectBorders(bool bordersImage[IMAGE_SIZE][IMAGE_SIZE], int chessBoard[IMAGE_SIZE][IMAGE_SIZE]) {
     for (int pixelY = 1; pixelY < IMAGE_SIZE; pixelY ++) {
         for (int pixelX = 1; pixelX < IMAGE_SIZE; pixelX ++) {
             // TODO: Improve this.
@@ -101,15 +90,44 @@ int main(int argc, char** argv) {
                             || chessBoard[pixelY][pixelX] != chessBoard[pixelY][pixelX-1];
         }
     }
+}
+
+int main(int argc, char** argv) {
+    srand(time(NULL)) ;
+    // Compute coordinates of the corners of the virtual screen.
+    glm::vec3 dir = glm::normalize(-EYE);
+    glm::vec3 left = glm::normalize(glm::cross(dir, glm::vec3(0.0f, 0.0f, 1.0f)));
+    glm::vec3 up = glm::normalize(glm::cross(dir, left));
+    glm::vec3 topLeft = EYE + dir - left - up;
+    glm::vec3 topRight = EYE + dir + left - up;
+    glm::vec3 bottomLeft = EYE + dir - left + up
+
+    // Cast rays onto function and generate "chess board" image.
+    int chessBoard[IMAGE_SIZE][IMAGE_SIZE];
+    rayTracing(chessBoard, EYE, topLeft, topRight, bottomLeft);
+    int chessBoardRightEye[IMAGE_SIZE][IMAGE_SIZE];
+    rayTracing(chessBoardRightEye,
+            EYE + left * EYE_FACTOR,
+            topLeft + left * EYE_FACTOR,
+            topRight + left * EYE_FACTOR,
+            bottomLeft + left * EYE_FACTOR);
+  
+    // Detect borders on "chess board".
+    bool bordersImage[IMAGE_SIZE][IMAGE_SIZE];
+    detectBorders(bordersImage, chessBoard);
+    bool bordersImageRightEye[IMAGE_SIZE][IMAGE_SIZE];
+    detectBorders(bordersImageRightEye, chessBoardRightEye);
 
     // Save bordersImage to file.
     printf("P2\n");
-    printf("%d %d\n", IMAGE_SIZE, IMAGE_SIZE);
+    printf("%d %d\n", IMAGE_SIZE * 2, IMAGE_SIZE);
     printf("1\n");
     for (int i = 0; i < IMAGE_SIZE; i ++) {
       for (int j = 0; j < IMAGE_SIZE; j ++) {
-//        printf("%d ", chessBoard[i][j] ? 0 : 1);
         printf("%d ", bordersImage[i][j] ? 0 : 1);
+      }
+      for (int j = 0; j < IMAGE_SIZE; j ++) {
+        printf("%d ", bordersImageRightEye[i][j] ? 0 : 1);
       }
       printf("\n");
     }
